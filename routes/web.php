2d2a4c4748ae2cccc88d7f222fb2a51686bcf4fe
Bartolomeus H.A <manktuber@gmail.com>
2024-01-03 00:11:35 +0700

    commit 2 back end

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,52 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+Route::get('/home', [HomeController::class, 'home'])->name('rumah');
+
+// Route::get('/jual', function () {
+//     return view('page_jual_beli.jual_page');
+// });
+
+// Route::get('/beli', function () {
+//     return view('page_jual_beli.beli_page');
+// });
+
+// create book //
+// Page khusus get page create book
+// pakein middleare agar user role customer gk bisa masuk ke page ini
+
+// Route::middleware(['auth', 'CreateBookPageMiddleware'])->group(function () {
+//     // GET
+//     // create book page
+//     Route::get('/create-book', [BookController::class, 'redirectToCreateBookPage']);
+
+//     // post create book
+//     Route::post('/post-create-book', [BookController::class, 'createBook']);
+
+//     // update //
+//     // kita pasing id untuk page update agar si aplikasi tau book apa yang ingin kita update
+//     Route::get('/update-book-page/{id}', [BookController::class, 'updateBookPage']);
+
+//     // data yang bakal di post khusus update data dari table book
+//     Route::post('/update-book/{id1}', [BookController::class, 'updateBook']);
+
+
+//     // delete
+//     // kita pasing id untuk delete agar si aplikasi tau book apa yang ingin kita delete dari primary key
+//     Route::post('/delete-book/{id}', [BookController::class, 'deleteBook']);
+// });
+
+Route::post('/send-mail-user-info', [EmailController::class, 'SendMail'])->name('SendMail')->middleware('auth');
